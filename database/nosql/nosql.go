cd2bb3ecb4b0fc30004d3157b641c05582920ffd
jRFlint <flint@77i.su>
2020-03-15 18:19:08 +0300

    split

--- database/nosql/nosql.go
+++ database/nosql/nosql.go
@@ -5,23 +5,46 @@ package nosql
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 )
+
+// OpenTXMethod is callback functon to open transaction in NoSQL object
+type OpenTXMethod func() (*sql.Tx, error)
 
 // NoSQL object
 type NoSQL struct {
-	openMethod func() (*sql.Tx, error)
+	openMethod OpenTXMethod
 }
 
-// Query execute request to database with command and paramerers map
+// New is NoSQL object constructor
+func New(openMethod OpenTXMethod) *NoSQL {
+	return &NoSQL{openMethod}
+}
+
+// Query execute request to database with command and data interface. It called function command(data).
+// Function should return json value
 func (_self *NoSQL) Query(command string, data interface{}) (res interface{}, err error) {
-	// convert params to json
+	// convert data to raw json
 	var raw []byte
 	if raw, err = json.Marshal(data); err != nil {
 		return
 	}
 	// open tx
 	var tx *sql.Tx
-	if tx, err = _self.openMethod(); err != nil {
-		return
+	if tx, err = _self.openMethod(); err == nil {
+		// execute query and scan result
+		var rRaw []byte
+		row := tx.QueryRow(fmt.Sprintf("select * from %v($1)", command), raw)
+		if err = row.Scan(&rRaw); err != nil {
+			tx.Rollback()
+			return
+		}
+		// convert raw result data to json and return
+		if err = json.Unmarshal(rRaw, &res); err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
 	}
+	return
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fcg-xvii/go-tools
 
 go 1.13
+
+require github.com/lib/pq v1.3.0
