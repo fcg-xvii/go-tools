0c9739afafd15649e90bba7750a58a8cdbd62f94
Vladimir V Maksimov <flint@77i.su>
2020-05-03 12:29:22 +0300

    in progress

---
+++ text/audio/speechkit/yask/yask.go
@@ -0,0 +1,46 @@
+package yask
+
+import (
+	"fmt"
+	"io"
+	"net/url"
+	"strconv"
+)
+
+const (
+	YaSTTUrl   = "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize"
+	FormatLPCM = "lpcm"
+	FormatOgg  = "oggopus"
+	Rate8k     = "8000"
+	Rate16k    = "16000"
+	Rate48k    = "48000"
+)
+
+type STTConfig struct {
+	Lang            string
+	Topic           string
+	ProfanityFilter bool
+	Format          string
+	Rate            string
+	YaFolderID      string
+	YaAPIKey        string
+	Data            io.Reader
+}
+
+func (s *STTConfig) URI() sring {
+	vals := url.Values{
+		"lang":            s.Lang,
+		"topic":           s.Topic,
+		"profanityFilter": strconv.FormatBool(s.ProfanityFilter),
+		"format":          s.Format,
+		"simpleRateHertz": s.Rate,
+		"folderId":        s.YaFolderID,
+	}
+
+	url := fmt.Sprintf("%v?%v", YaSTTUrl, vals.Encode())
+	return url
+}
+
+func SpeechToTextShort() {
+
+}
