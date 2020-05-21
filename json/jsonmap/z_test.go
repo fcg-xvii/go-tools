91eca879ada31472a453b9c78210ab7e4710d95c
Vladimir V Maksimov <flint@77i.su>
2020-05-21 21:54:07 +0300

    json null test

--- json/jsonmap/z_test.go
+++ json/jsonmap/z_test.go
@@ -7,8 +7,9 @@ import (
 func TestJSON(t *testing.T) {
 	m := JSONMap{
 		"jsrc": []int{1, 2, 3, 4},
+		"kate": nil,
 	}
-
+	t.Log(m, string(m.JSON()))
 	t.Log(string(m.ValueJSON("jsrc", []byte{})))
 	t.Log(string(m.ValueJSON("jsrc1", []byte("{ }"))))
 }
