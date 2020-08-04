bc86af84910e1db9d56ec6a74ef925e42a231fab
Vladimir V Maksimov <flint@77i.su>
2020-08-04 14:28:07 +0300

    cachemap

--- cache/z_test.go
+++ cache/z_test.go
@@ -1,7 +1,6 @@
 package cache
 
 import (
-	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -15,31 +14,5 @@ func TestMap(t *testing.T) {
 	log.Println(m.Get("key"))
 	m.Delete("key")
 	log.Println(m.Get("key"))
-	log.Println(m.Get("key1"))
-
-	go func() {
-		for i := 0; i < 1000; i++ {
-			go func(j int) {
-				m.GetOrCreate(fmt.Sprintf("key%v", j), func(interface{}) (interface{}, bool) {
-					//log.Println("CREATE_CALL")
-					return 100, true
-				})
-			}(i)
-			time.Sleep(time.Millisecond * 5)
-			//log.Println("i", i)
-		}
-
-	}()
-
-	time.Sleep(time.Second * 3)
-	log.Println("!!!")
-	for {
-		log.Println("len", m.Len())
-		/*if m.Len() == 0 {
-			break
-		}*/
-		time.Sleep(time.Millisecond * 500)
-	}
-
 	log.Println(m.Get("key1"))
 }
