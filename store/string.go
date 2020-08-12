645386de6cacecbc9aad1864b3f98a963cdaec4d
Vladimir V Maksimov <flint@77i.su>
2020-08-12 14:27:49 +0300

    string added

---
+++ store/string.go
@@ -0,0 +1,145 @@
+package store
+
+import "sync"
+
+type StringCallCreate func(key string) (value interface{}, created bool)
+type StringCallCheck func(key string, value interface{}, exists bool) (rKey string, rValue interface{}, created bool)
+
+func StringFromMap(m map[string]interface{}) *StoreString {
+	return &StoreString{
+		locker: new(sync.RWMutex),
+		items:  m,
+	}
+}
+
+func StringNew() *StoreString {
+	return &StoreString{
+		locker: new(sync.RWMutex),
+		items:  make(map[string]interface{}),
+	}
+}
+
+type StoreString struct {
+	locker *sync.RWMutex
+	items  map[string]interface{}
+}
+
+func (s *StoreString) delete(key string) {
+	delete(s.items, key)
+}
+
+func (s *StoreString) Delete(key string) {
+	s.locker.Lock()
+	delete(s.items, key)
+	s.locker.Unlock()
+}
+
+func (s *StoreString) DeleteMulti(keys []string) {
+	s.locker.Lock()
+	for _, key := range keys {
+		delete(s.items, key)
+	}
+	s.locker.Unlock()
+}
+
+func (s *StoreString) set(key string, val interface{}) {
+	s.items[key] = val
+}
+
+func (s *StoreString) Set(key string, val interface{}) {
+	s.locker.Lock()
+	s.set(key, val)
+	s.locker.Unlock()
+}
+
+func (s *StoreString) SetMulti(m map[string]interface{}) {
+	s.locker.Lock()
+	for key, val := range m {
+		s.set(key, val)
+	}
+	s.locker.Unlock()
+}
+
+func (s *StoreString) get(key string) (val interface{}, check bool) {
+	val, check = s.items[key]
+	return
+}
+
+func (s *StoreString) Get(key string) (val interface{}, check bool) {
+	s.locker.RLock()
+	val, check = s.get(key)
+	s.locker.RUnlock()
+	return
+}
+
+func (s *StoreString) GetCreate(key string, mCreate StringCallCreate) (res interface{}, check bool) {
+	if res, check = s.Get(key); !check {
+		s.locker.Lock()
+		if res, check = s.get(key); check {
+			s.locker.Unlock()
+			return
+		}
+
+		if res, check = mCreate(key); check {
+			s.set(key, res)
+		}
+		s.locker.Unlock()
+	}
+	return
+}
+
+func (s *StoreString) GetCheck(key string, mCheck StringCallCheck) (res interface{}, check bool) {
+	s.locker.Lock()
+	res, check = s.get(key)
+	if rKey, rVal, rCheck := mCheck(key, res, check); rCheck {
+		s.set(rKey, rVal)
+		key, res, check = rKey, rVal, true
+	}
+	s.locker.Unlock()
+	return
+}
+
+// Each implements a map bypass for each key using the callback function. If the callback function returns false, then the cycle stops
+func (s *StoreString) Each(callback func(string, interface{}) bool) {
+	s.locker.RLock()
+	for key, val := range s.items {
+		if !callback(key, val) {
+			s.locker.RUnlock()
+			return
+		}
+	}
+	s.locker.RUnlock()
+}
+
+func (s *StoreString) Len() (res int) {
+	s.locker.RLock()
+	res = len(s.items)
+	s.locker.RUnlock()
+	return
+}
+
+func (s *StoreString) Keys() (res []string) {
+	s.locker.RLock()
+	res = make([]string, 0, len(s.items))
+	for key := range s.items {
+		res = append(res, key)
+	}
+	s.locker.RUnlock()
+	return
+}
+
+func (s *StoreString) Clear() {
+	s.locker.Lock()
+	s.items = make(map[string]interface{})
+	s.locker.Unlock()
+}
+
+func (s *StoreString) Map() (res map[string]interface{}) {
+	res = make(map[string]interface{})
+	s.locker.RLock()
+	for key, val := range s.items {
+		res[key] = val
+	}
+	s.locker.RUnlock()
+	return
+}
--- store/z_test.go
+++ store/z_test.go
@@ -32,3 +32,31 @@ func TestStore(t *testing.T) {
 
 	log.Println(st.Map())
 }
+
+func TestStoreString(t *testing.T) {
+	st := StringNew()
+
+	st.Set("one", 10)
+
+	log.Println(st.Get("one"))
+	log.Println(st.Get("two"))
+
+	log.Println(st.GetCreate("two", func(key string) (value interface{}, created bool) {
+		return 20, true
+	}))
+
+	st.Delete("two")
+
+	log.Println(st.GetCreate("two", func(key string) (value interface{}, created bool) {
+		return 30, true
+	}))
+
+	log.Println(st.Map())
+
+	st = StringFromMap(map[string]interface{}{
+		"one": 1,
+		"two": 2,
+	})
+
+	log.Println(st.Map())
+}
