50187d10e4eaff15494e2ac3d8b508cf7d29af78
jrFlint <flint@77i.su>
2021-06-26 10:36:02 +0300

    bugfix

--- json/json.go
+++ json/json.go
@@ -14,6 +14,10 @@ func Marshal(v interface{}) ([]byte, error) {
 	return json.Marshal(v)
 }
 
+func MarshalIndent(v interface{}, prefix, indent string) ([]byte, error) {
+	return json.MarshalIndent(v, prefix, indent)
+}
+
 func Unmarshal(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
 }
