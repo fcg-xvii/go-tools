bf080e8ec684db3e5e80d0508e3a8f151961bfa2
Vladimir V Maksimov <flint@77i.su>
2021-07-24 18:16:39 +0300

    Unmarshal

--- json/json.go
+++ json/json.go
@@ -2,7 +2,9 @@ package json
 
 import (
 	"encoding/json"
+	"io"
 	"log"
+	"os"
 )
 
 func Log(v interface{}) {
@@ -20,4 +22,18 @@ func MarshalIndent(v interface{}, prefix, indent string) ([]byte, error) {
 
 func Unmarshal(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
+}
+
+func UnmarshalReader(r io.Reader, v interface{}) error {
+	return Decode(r, v)
+}
+
+func UnmarshalFile(fileName string, v interface{}) (err error) {
+	var f *os.File
+	if f, err = os.Open(fileName); err != nil {
+		return
+	}
+	err = Decode(f, v)
+	f.Close()
+	return
 }
